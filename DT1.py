ad9b2cef9c4d48da286a402daef558c229e75bc1
meitarhalfon <39064149+meitarhalfon@users.noreply.github.com>
2020-01-15 16:19:05 +0200

    Add files via upload

---
+++ DT1.py
@@ -0,0 +1,8 @@
+# imports
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from sklearn import tree
+from sklearn.preprocessing import StandardScaler
+from sklearn.model_selection import train_test_split
+
