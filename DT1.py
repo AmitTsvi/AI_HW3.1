0ec8a0e22482a8fccffc65abaa10644800bef1fb
AmitTsvi <33572533+AmitTsvi@users.noreply.github.com>
2020-01-18 17:25:54 +0200

    Delete DT1.py

--- DT1.py
+++
@@ -1,8 +0,0 @@
-# imports
-import numpy as np
-import pandas as pd
-import matplotlib.pyplot as plt
-from sklearn import tree
-from sklearn.preprocessing import StandardScaler
-from sklearn.model_selection import train_test_split
-
